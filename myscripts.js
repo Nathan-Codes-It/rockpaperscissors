2a92fbf872bd99f0094306cefc3aebee0d227c16
Nathan <casperson24315@lcsd2kids.org>
2021-04-08 09:47:13 -0600

    got help with a better, simple code. i was following an old tutorial because i had gotten lost with the odin project tutorial

--- myscripts.js
+++ myscripts.js
@@ -1,60 +1,57 @@
-/*let userChoice = prompt("Do you choose rock, paper or scissors?");
-let computerChoice = Math.random();
-if (computerChoice < 0.34) {
-    computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
-    computerChoice = "paper";
-} else {
-    computerChoice = "scissors";
-}
+let userChoice = prompt("Do you choose rock, paper or scissors?");
 
-let compare = function(choice1, choice2) {
-    if(choice1 === choice2) {
-    return "The result is a tie!";
-}
-if(choice1 === "rock") {
-    if(choice2 === "scissors") {
-        return "rock wins";
+function computerPlay(){
+   let   computerChoice = Math.random();
+    if (computerChoice < 0.34) {
+        computerChoice = "rock";
+       
+    } else if(computerChoice <= 0.67) {
+        computerChoice = "paper";
     } else {
-        return "paper wins";
-    }
+        computerChoice = "scissors";
+    } 
+    return computerChoice;
 }
-if(choice1 === "paper") {
-    if(choice2 === "rock") {
-        return "paper wins";
-    } else {
-        if(choice2 === "scissors") {
-            return "scissors wins";
-    }
-}
-if(choice1 === "scissors") {
-    if(choice2 === "rock") {
-        return "rock wins";
-    } else {
-        if(choice2 === "paper") {
-            return "scissors wins";
-        }
-    }
-}
-}
-};
-console.log("User Choice: " + userChoice);
-console.log("Computer Choice: " + computerChoice);
-compare(userChoice, computerChoice);*/
+function gamePlay(playerChoice, computerChoice){
+    console.log(playerChoice.toUpperCase());
+    console.log(computerChoice);
 
-function playRound(playerSelection, computerSelection) {
-    let yourChoice = prompt("Whats it gonna be? rock, paper, or scissors?");
-    let computer = Math.random();
-    if (computer < 0.40) {
-        computer = "rock";
-    } else if (computer <= 0.60){
-        computer = "paper";
 
-    } else {
-        computer = "scissors"
-    }
-  }
-  
-  const playerSelection = "rock";
-  const computerSelection = computerPlay();
-  console.log(playRound(playerSelection, computerSelection));
+}
+gamePlay(userChoice, computerPlay())
+
+
+// var compare = function(choice1, choice2) {
+//     if(choice1 === choice2) {
+//     return "The result is a tie!";
+// }
+// if(choice1 === "rock") {
+//     if(choice2 === "scissors") {
+//         return "rock wins";
+//     } else {
+//         return "paper wins";
+//     }
+// }
+// if(choice1 === "paper") {
+//     if(choice2 === "rock") {
+//         return "paper wins";
+//     } else {
+//         if(choice2 === "scissors") {
+//             return "scissors wins";
+//     }
+// }
+// }
+// if(choice1 === "scissors") {
+//     if(choice2 === "rock") {
+//         return "rock wins";
+//     } else {
+//         if(choice2 === "paper") {
+//             return "scissors wins";
+//         }
+//     }
+// }
+// }
+// ;
+// console.log("User Choice: " + userChoice);
+// console.log("Computer Choice: " + computerChoice);
+// compare(userChoice, computerChoice);
