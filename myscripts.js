36f619211ff209393be5f441ddb47f61dca2a3c0
Nathan <casperson24315@lcsd2kids.org>
2021-04-19 09:20:42 -0600

    score function added, got to make sure it adds 1 to the score value every time player wins, i havent gotten that to work yet

--- myscripts.js
+++ myscripts.js
@@ -1,7 +1,7 @@
 
 //let userChoice = prompt("rock, paper, or scissors? (also remember to spell correctly");
 function computerPlay(){
-    let score = 0;
+    
    let   computerChoice = Math.random();
     if (computerChoice < 0.40) {
         computerChoice = "rock";
@@ -14,67 +14,71 @@ function computerPlay(){
     return computerChoice.toLowerCase();
 }
 function gamePlay(){
-    let score = 0;
+    //let score = 0;
 
     let playerChoice = prompt("pick between rock, paper, and scissors");
     let computerChoice = computerPlay;
     console.log(singleRound(playerChoice, computerChoice()));
-    score += (singleRound(playerChoice, computerChoice()))
+   // score += (singleRound(playerChoice, computerChoice()))
 
 
 
      playerChoice = prompt("pick between rock, paper, and scissors");
      computerChoice = computerPlay;
     console.log(singleRound(playerChoice, computerChoice()));
-    score += (singleRound(playerChoice, computerChoice()))
+   // score += (singleRound(playerChoice, computerChoice()))
 
      playerChoice = prompt("pick between rock, paper, and scissors");
     computerChoice = computerPlay;
     console.log(singleRound(playerChoice, computerChoice()));
-    score += (singleRound(playerChoice, computerChoice()))
+    //score += (singleRound(playerChoice, computerChoice()))
 
 
      playerChoice = prompt("pick between rock, paper, and scissors");
      computerChoice = computerPlay;
     console.log(singleRound(playerChoice, computerChoice()));
-    score += (singleRound(playerChoice, computerChoice()))
+    //score += (singleRound(playerChoice, computerChoice()))
 
 
      playerChoice = prompt("pick between rock, paper, and scissors");
      computerChoice = computerPlay;
     console.log(singleRound(playerChoice, computerChoice()));
-    score += (singleRound(playerChoice, computerChoice()))
+    //score += (singleRound(playerChoice, computerChoice()))
 }
 
 
 
   function singleRound(userChoice, computerChoice, score){
+      score = 0;
       if(userChoice == computerChoice){
-        
+    console.log(score);
           return `Tie! player: ${userChoice} computer: ${computerChoice} `;
       }
       else if(userChoice == "rock" && computerChoice == "paper") {
-        
+        console.log(score);
           return `loss! player: ${userChoice} computer: ${computerChoice}`;
       }
-      else if(userChoice == "paper" && computerChoice == "rock") {
-        
+      else if(userChoice == "paper" && computerChoice == "rock" ) {
+        score++;
+        console.log(score);
           return `win! player: ${userChoice} computer: ${computerChoice}`;
-      }
+          }
       else if(userChoice == "scissors" && computerChoice =="rock"){
-       
+        console.log(score);
           return `loss! player: ${userChoice} computer: ${computerChoice}`;
       }
       else if(userChoice == "rock" && computerChoice == "scissors"){
-        
+        score++;
+        console.log(score);
           return `win! player: ${userChoice} computer: ${computerChoice}`;
       }
       else if(userChoice == "paper" && computerChoice == "scissors"){
-        
+        console.log(score);
           return `loss! player: ${userChoice} computer: ${computerChoice}`;
       }
       else if(userChoice == "scissors" && computerChoice == "paper"){
-      
+        score++;
+        console.log(score);
           return `win! player: ${userChoice} computer: ${computerChoice}`;
       }
   }
